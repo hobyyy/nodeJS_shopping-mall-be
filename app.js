f6682e1c594f7c590b2577829a14c91bb5c3f333
Eunseo Kim <dmstj9408@gamil.com>
2024-10-25 13:47:55 +0900

    [create]router-setting

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const indexRouter = require('./routes/index');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())  // req.body가 객ㅔ로 인식이 되기때문에
+app.use(bodyParser.json())  // req.body가 객체로 인식이 되기때문에
 
-const mongoURI = process.env.LOCAL_DB_ADDRESS;
+app.use('/api', indexRouter)
+
+// const mongoURI = process.env.LOCAL_DB_ADDRESS;
+const mongoURI = process.env.PROD_DB_ADDRESS;
 mongoose
   .connect(mongoURI)
   .then(() => console.log('mongoose connected'))
   .catch((error) => console.log('DB connected fail',error));
 
-app.listen(process.env.PORT || 5000,() => {
-  console.log('Server ON',);
+app.listen(PORT,() => {
+  console.log(`Server ON ${PORT}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const userAPI = require('./user.api');
+
+router.use('/user', userAPI);
+
+module.exports = router;
