f5e63715c06cd82e743bb9ae80833555da9c2937
EunseoKim <dmstj9408@gmail.com>
2024-10-30 20:35:16 +0900

    [feature]search할 때 쿼리 추가

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,7 +15,18 @@ productController.createProduct = async(req,res) => {
 
 productController.getProducts = async(req,res) => {
   try {
-    const products = await Product.find({});
+    const {page, name} = req.query;
+    // if(name) {
+    //   // $regex:name ; name이 포함된
+    //   // $options:'i' ; 대소문자 구별없이
+    //   const products = await Product.find({name: {$regex:name, $options:'i'}});
+    // }else {
+    //   const products = await Product.find({});
+    // }
+    // => 검색조건 모두 합쳐보자
+    const cond = name ? {name: {$regex:name, $options:'i'}} : {};
+    let query = Product.find(cond)
+    const products = await query.exec(); // query를 따로 실행
     res.status(200).json({status: 'success', data: products})
   }catch(error) {
     res.status(400).json({status: 'fail', error: error.message})
