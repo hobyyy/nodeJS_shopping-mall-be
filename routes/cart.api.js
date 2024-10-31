c870e178087c8973c4ad5937516c89f6ef2e2030
EunseoKim <dmstj9408@gmail.com>
2024-11-01 03:43:56 +0900

    [hotfix]카트 페이지에서 삭제 안되는 기능 수정

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -5,6 +5,8 @@ const cartController = require('../controllers/cart.controller');
 
 router.post('/', authController.authenticate, cartController.addItemToCart);
 router.get('/', authController.authenticate, cartController.getCart);
-router.put("/:id", authController.authenticate, cartController.editCartItem);
+router.delete('/:id', authController.authenticate, cartController.deleteCartItem);
+router.put('/:id', authController.authenticate, cartController.editCartItem);
+router.get('/qty', authController.authenticate, cartController.getCartQty);
 
 module.exports = router;
