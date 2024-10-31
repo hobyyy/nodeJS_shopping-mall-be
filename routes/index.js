c22914cefa05557f56aba72b2f2ea994042393b7
EunseoKim <dmstj9408@gmail.com>
2024-11-01 00:28:11 +0900

    [feature]카트 첫 생성 & 카트에 에 아이템 추가 기능

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const userAPI = require('./user.api');
 const authAPI = require('./auth.api');
 const productAPI = require('./product.api');
+const cartAPI = require('./cart.api');
 
 router.use('/user', userAPI);
 router.use('/auth', authAPI);
 router.use('/product', productAPI);
+router.use('/cart', cartAPI);
 
 module.exports = router;
