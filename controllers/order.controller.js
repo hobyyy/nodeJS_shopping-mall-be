ad8f11bff84dc1bd49bc5140d112c2f350919027
Eunseo Kim <dmstj9408@gamil.com>
2024-11-04 12:46:50 +0900

    [create]order 기능 생성

---
+++ controllers/order.controller.js
@@ -0,0 +1,42 @@
+const orderController = {};
+const Order = require('../models/Order');
+const productController = require('./product.controller');
+const randomStringGenerator = require('../utils/randomStringGenerator');
+
+orderController.createOrder = async(req,res) => {
+  try {
+    // fe에서 데이터 보낸거 받아오기
+    // userId, shipTo, contact, totalPrice, orderList
+    const {userId} = req;
+    const {shipTo, contact, totalPrice, orderList} = req.body;
+
+    // 재고 확인 & 업데이트
+    const insufficientStockItems = await productController.checkItemListStock(orderList); // 재고가 불충분한 item list 받기
+
+    // 재고가 충분하지 않은 item이 있으면 => error
+    if(insufficientStockItems.length > 0) {
+      const errorMessage = insufficientStockItems.reduce(
+        (total,item) => total += item.message, 
+        '' // string형으로 반환
+      );
+      throw new Error(errorMessage)
+    }
+    
+    // 모든 item이 재고가 충분했으면 order를 만들기
+    const newOrder = new Order({
+      userId,
+      totalPrice,
+      shipTo,
+      contact,
+      items: orderList,
+      orderNum: randomStringGenerator() // random한 번호를 뽑아서 orderNum 지정
+    })
+    await newOrder.save();
+
+    res.status(200).json({status: 'success', orderNum: newOrder.orderNum});
+  } catch (error) {
+    res.status(400).json({status: 'fail', error: error.message});
+  }
+};
+
+module.exports = orderController;
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const userAPI = require('./user.api');
 const authAPI = require('./auth.api');
 const productAPI = require('./product.api');
 const cartAPI = require('./cart.api');
+const orderAPI = require('./order.api');
 
 router.use('/user', userAPI);
 router.use('/auth', authAPI);
 router.use('/product', productAPI);
 router.use('/cart', cartAPI);
+router.use('/order', orderAPI);
 
 module.exports = router;
